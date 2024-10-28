37b580d91a7477bdcf17ffd4d16a908b8ae73894
James Collins <jcollins@addgene.org>
2024-10-28 14:40:51 -0400

    first version of tincan-tube vpn client code

---
+++ client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"tincan-tube/server/vpn"
+)
+
+func main() {
+	serverAddr := "vpn_server:51820" // this is the Docker compose service name
+	conn, err := net.Dial("udp", serverAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	privateKey := vpn.GeneratePrivateKey()
+	publicKey := vpn.DerivePublicKey(privateKey)
+
+	encryptedMessage := vpn.EncryptMessage([]byte("Hello, VPN Server!"), publicKey)
+	_, err = conn.Write(encryptedMessage)
+	if err != nil {
+		fmt.Println("Error sending packet:", err)
+	}
+
+	buffer := make([]byte, 1500)
+	n, _, err := conn.ReadFrom(buffer)
+	if err != nil {
+		fmt.Println("Error reading packet:", err)
+	}
+
+	decryptedResponse := vpn.DecryptMessage(buffer[:n], privateKey)
+	fmt.Printf("Response from server: %s\n", decryptedResponse)
+}
